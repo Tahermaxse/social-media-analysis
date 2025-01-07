22eeea8fcbfdff7c59baf0013b3bbc6bafcf9544
Tahermaxse <hathitaher83@gmail.com>
2025-01-07 23:45:52 +0530

    production fix

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -3,6 +3,6 @@ import { NextResponse } from "next/server";
 import { createChatAPIRoute } from "@/lib/chat/api";
 
 export async function POST(req: Request) {
-    const { status, data } = await createChatAPIRoute(req);
+    const { status, data } = await createChatAPIRoute(req) as { status: number, data: any };
     return NextResponse.json(data, { status });
 }
